e412eef0d0175947558f08c64019f75dd1ab0264
Omar Badran <omarbadran.dev@gmail.com>
2019-09-13 02:00:19 +0300

    Setup Gulp

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const gulp = require('gulp');
+
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+
+/**
+ * Files
+ */
+const files = { 
+    sass: {
+        src:    'assets/scss/**/*.scss',
+        dist:   'assets/css'
+    },
+    js: {
+        src:    ['assets/js/**/*.js', '!assets/js/app.min.js'],
+        dist:   'assets/js'
+    }
+}
+
+/**
+ * Compile SASS
+ */
+function styles () {    
+    return gulp.src(files.sass.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([ autoprefixer(), cssnano() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(files.sass.dist)
+    );
+}
+
+
+/**
+ * Compile javascript
+ */
+function scripts () {
+    return gulp.src(files.js.src)
+        .pipe(concat('app.min.js'))
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))      
+        .pipe(uglify())
+        .pipe(gulp.dest(files.js.dist)
+    );
+}
+
+
+/**
+ * Watch for changes
+ */
+function watch () {
+    gulp.watch(files.sass.src, styles);
+    gulp.watch(files.js.src, scripts);
+}
+
+
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch;
