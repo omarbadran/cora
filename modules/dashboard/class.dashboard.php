7f35ab86648bf6855ed380b24aba09ca640fa731
Omar Badran <omarbadran.dev@gmail.com>
2019-09-17 14:31:21 +0300

    Add Dashboard Module

---
+++ modules/dashboard/class.dashboard.php
@@ -0,0 +1,40 @@
+<?php
+
+# Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Dashboard Class
+ * 
+ * 
+ * @since 1.0.0
+ * @author Omar Badran
+ */
+class Cora_Dashboard {
+    
+    /**
+     * Options framework class.
+     *
+     * @since       1.0.0
+     * @access      public
+     * @var         object
+     */
+    public $options;
+
+    /**
+     * Class constructor.
+     *
+     * @since       1.0.0
+     * @access      public
+     * @return      void
+     */
+    public function __construct( $options ) {
+        
+        # Load Options
+        $this->options = $options;
+
+        require_once dirname(__FILE__) . "/options.php";
+
+    }
+
+}
---
+++ modules/dashboard/options.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Dashboard options
+ * 
+ * @since 1.0.0
+ * @author Omar Badran
+*/
+
+# Add Section
+$this->options->add_section([
+    'id'        =>  'dashboard',
+    'title'     =>  esc_html__('Dashboard' , 'cora'),
+    'icon'      =>  'dns',
+]);
+
+# Add Section
+$this->options->add_field([
+    'id'            =>  'widgets',
+    'section'       =>  'dashboard',
+    'title'         =>  esc_html__('Add Widgets' , 'cora'),
+    'type'          =>  'repeater',
+    'fields'        =>  [
+        [
+            'id'        =>  'title',
+            'title'     =>  esc_html__('Title' , 'cora'),
+            'type'      =>  'text',
+        ],
+        [
+            'id'    =>  'content',
+            'title' =>  esc_html__('Content' , 'cora'),
+            'type'  =>  'editor',
+        ],
+    ],
+    'new_item_default'  => [
+        'title'         =>  esc_html__('Custom Widget', 'cora'),
+    ],
+]);
