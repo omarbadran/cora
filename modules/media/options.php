722e544e825b897971039240882545449646dd24
Omar Badran <omarbadran.dev@gmail.com>
2019-09-17 14:27:50 +0300

    Add Media Module

---
+++ modules/media/options.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Media options
+ * 
+ * @since 1.0.0
+ * @author Omar Badran
+*/
+
+# Add Section
+$this->options->add_section([
+    'id'        =>  'media',
+    'title'     =>  esc_html__('Media' , 'cora'),
+    'icon'      =>  'photo',
+]);
+
+# SVG Support
+$this->options->add_field([
+    'id'            =>  'svg_support',
+    'section'       =>  'media',
+    'title'         =>  esc_html__('SVG Support' , 'cora'),
+    'type'          =>  'switch',
+    'default'       =>  true,
+]);
+
+# ICO Support
+$this->options->add_field([
+    'id'            =>  'ico_support',
+    'section'       =>  'media',
+    'title'         =>  esc_html__('ICO Support' , 'cora'),
+    'type'          =>  'switch',
+    'default'       =>  true,
+]);
